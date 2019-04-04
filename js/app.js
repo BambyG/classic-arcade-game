0f9fca7953ab6952b51b2d5ca09f3ea8e3057d86
Bamby <bamby.gassama@gmail.com>
2019-04-04 14:31:35 +0200

    Add existing file

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
-// Enemies our player must avoid
-var Enemy = function() {
+/// Enemies our player must avoid
+var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x
+    this.y= y
+    this.speed = speed
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
-
+ 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+  
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -44,3 +47,36 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
+
+///// Bambys code 
+
+// Enemies our player must avoid
+class Enemy {
+    constructor(x,y,speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started        
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images        
+    this.sprite = 'images/enemy-bug.png';
+    }
+}
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    this.x = x*dt
+    this.y = y*dt
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
