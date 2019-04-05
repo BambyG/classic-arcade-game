9aa295b8ffe0f4cbde5d5a3685e886c16fa19f0a
Bamby <bamby.gassama@gmail.com>
2019-04-05 07:51:21 +0200

    work it

--- js/app.js
+++ js/app.js
@@ -64,6 +64,9 @@ class Enemy {
     // a helper we've provided to easily load images        
     this.sprite = 'images/enemy-bug.png';
     }
+
+
+
 }
 
 // Update the enemy's position, required method for game
@@ -80,3 +83,73 @@ Enemy.prototype.update = function(dt) {
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+class Player {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.spirite= 'images/char-pink-girl.png';
+    }
+
+    update(dt){
+        this.x = x*dt
+        this.y = y*dt
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(){
+
+    }
+
+
+
+    
+
+}
+
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
+
+
+
+
+let player = new Player(50, 135,220);
+
+
+let enemiesTotal = []
+
+let enemy1 = new Enemy(-90, 60,55);
+let enemy2 = new Enemy(-190, 140,55);
+let enemy3 = new Enemy(-290, 230,55);
+let enemy4 = new Enemy(-390, 140,66);
+
+let allEnemies = [enemy1, enemy2, enemy3, enemy4];
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+    player.handleInput(allowedKeys[e.keyCode]);
+});
